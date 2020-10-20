f85eee41497b76c9a43224ebc358ebf0a2065501
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-20 14:35:04 +0530

    Welcome to employee payroll

---
+++ src/main/java/com/cg/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,34 @@
+package com.cg.employeepayroll;
+import java.util.*;
+public class EmployeePayrollService {
+   private List<EmployeePayrollData> employeePayrollList ;
+   public EmployeePayrollService(){}
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll program");
+        /*ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();*/
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee ID: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name: ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee Salary: ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id,name,salary));
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println("\nWriting Employee Payroll roaster to console\n" +employeePayrollList);
+    }
+
+}
